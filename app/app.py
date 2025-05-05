fdb8d1f7f0a5010fdd95fa03e6011b152fb7c947
Shaurya Mittal <136336857+shaurya174@users.noreply.github.com>
2025-05-05 16:09:53 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -84,9 +84,10 @@ st.markdown(
 st.title("📄 Resume Classifier App")
 st.write("Upload a resume and let the model predict the most suitable job role!")
 import joblib
+
+tf_object = joblib.load('../Models/tf_object.pkl')
 idf_object = joblib.load('../Models/idf_object.pkl')
-tf_object = joblib.load('..Models/tf_object.pkl')
-xgb_model = joblib.load('..Models/xgboost_model.pkl')
+xgb_model = joblib.load('../Models/xgboost_model.pkl')
 uploaded_file = st.file_uploader("Upload your resume (PDF only)", type=["pdf"])
 if uploaded_file is not None:
     import fitz  # PyMuPDF
